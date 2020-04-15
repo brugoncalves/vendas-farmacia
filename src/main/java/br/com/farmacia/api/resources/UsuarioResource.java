209d42ce3046420e57dee84fd2010635273fcf60
Bruna Gonçalves <brunabkg2@gmail.com>
2020-04-15 20:32:07 -0300

    Endpoint para inserir o usuario

--- src/main/java/br/com/farmacia/api/resources/UsuarioResource.java
+++ src/main/java/br/com/farmacia/api/resources/UsuarioResource.java
@@ -7,10 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.farmacia.entities.Usuario;
+import br.com.farmacia.services.exceptions.ErroAutenticacaoException;
 import br.com.farmacia.services.impl.UsuarioServiceImpl;
 
 @RestController
@@ -26,8 +29,21 @@ public class UsuarioResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping(value = "/{id}")
 	public ResponseEntity <Optional<Usuario>> findById(@PathVariable Long id){
 		Optional<Usuario> usuario = service.findById(id);
 		return ResponseEntity.ok().body(usuario);
 	}
+	
+	@PostMapping
+	public ResponseEntity autenticar(@RequestBody Usuario usuario) {
+		try {
+			Usuario usuarioAutenticado = service.autenticar(usuario.getEmail(), usuario.getSenha());
+			return ResponseEntity.ok(usuarioAutenticado);
+		} catch(ErroAutenticacaoException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
+	
 }
