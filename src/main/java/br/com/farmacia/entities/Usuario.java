2f201a2b1e2a0e123dc54ea70ecc61d0f9c49f69
Bruna Gonçalves <brunabkg2@gmail.com>
2020-04-15 21:56:08 -0300

    Mapeamento da entidade Usuario

--- src/main/java/br/com/farmacia/entities/Usuario.java
+++ src/main/java/br/com/farmacia/entities/Usuario.java
@@ -2,11 +2,21 @@ package br.com.farmacia.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_usuario")
 public class Usuario implements Serializable{
 
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
