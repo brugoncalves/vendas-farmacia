f97d27cd577dcd689dd9cd9b083e86034e708912
Bruna Gonçalves <brunabkg2@gmail.com>
2020-04-15 21:46:33 -0300

    Endpoint para cadastrar usuarios

--- src/main/java/br/com/farmacia/api/resources/UsuarioResource.java
+++ src/main/java/br/com/farmacia/api/resources/UsuarioResource.java
@@ -1,5 +1,6 @@
 package br.com.farmacia.api.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.farmacia.entities.Usuario;
 import br.com.farmacia.services.exceptions.ErroAutenticacaoException;
@@ -35,7 +37,7 @@ public class UsuarioResource {
 		return ResponseEntity.ok().body(usuario);
 	}
 	
-	@PostMapping
+	@PostMapping(value = "/login")
 	public ResponseEntity autenticar(@RequestBody Usuario usuario) {
 		try {
 			Usuario usuarioAutenticado = service.autenticar(usuario.getEmail(), usuario.getSenha());
@@ -44,6 +46,11 @@ public class UsuarioResource {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 	}
-	
-	
+	@PostMapping(value = "/cadastrar-usuario")
+	public ResponseEntity<Usuario> salvarUsuario(@RequestBody Usuario obj){
+		Usuario usuario = service.salvarUsuario(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
