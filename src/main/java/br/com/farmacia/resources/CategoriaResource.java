ae799d4cf3b63c266ace3869d72c0391187b59d5
Bruna Gonçalves <brunabkg2@gmail.com>
2020-05-04 20:38:36 -0300

    Find Categoria By Id

---
+++ src/main/java/br/com/farmacia/resources/CategoriaResource.java
@@ -0,0 +1,25 @@
+package br.com.farmacia.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.farmacia.entities.Categoria;
+import br.com.farmacia.services.CategoriaService;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+	@Autowired
+	private CategoriaService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Categoria> findById(@PathVariable Long id){
+		Categoria categoria = service.findById(id);
+		return ResponseEntity.ok().body(categoria);
+	}
+}
