64f7b98aef59e7d1c3716ba9e1adb192133826a5
Bruna Gonçalves <brunabkg2@gmail.com>
2020-05-05 18:14:55 -0300

    Exception Handler

--- src/main/java/br/com/farmacia/services/CategoriaService.java
+++ src/main/java/br/com/farmacia/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.farmacia.api.repositories.CategoriaRepository;
 import br.com.farmacia.entities.Categoria;
+import br.com.farmacia.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria findById(Long id) {
 		Optional<Categoria> categoria = repository.findById(id);
-		return categoria.orElse(null);
+		return categoria.orElseThrow( () -> 
+			new ObjectNotFoundException("Objeto não encontrado! ID: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
