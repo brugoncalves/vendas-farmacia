03ee98127b80ded294284a8789faa313b522d2ac
Bruna Gonçalves <brunabkg2@gmail.com>
2020-05-11 21:28:25 -0300

    Endpoint cliente

--- src/main/java/br/com/farmacia/resources/ClienteResource.java
+++ src/main/java/br/com/farmacia/resources/ClienteResource.java
@@ -2,6 +2,7 @@ package br.com.farmacia.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class ClienteResource {
 	@Autowired
 	private ClienteService service;
 	
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<Cliente> findById(@PathVariable Long id){
 		Cliente cliente = service.findById(id);
 		return ResponseEntity.ok().body(cliente);
