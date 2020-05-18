9f5e2c1009b8d535cb3636cb8d155fe72c7d6ab7
Bruna Gonçalves <brunabkg2@gmail.com>
2020-05-18 13:48:11 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/br/com/farmacia/services/CategoriaService.java
+++ src/main/java/br/com/farmacia/services/CategoriaService.java
@@ -5,10 +5,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.farmacia.api.repositories.CategoriaRepository;
 import br.com.farmacia.entities.Categoria;
+import br.com.farmacia.entities.dto.CategoriaDTO;
 import br.com.farmacia.services.exceptions.DataIntegrityException;
 import br.com.farmacia.services.exceptions.ObjectNotFoundException;
 
@@ -46,4 +50,15 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
 		}
 	}
+	
+	public Page<Categoria> findByPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDto(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	
 }
